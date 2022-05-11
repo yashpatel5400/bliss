2ad4c22e9d8a3dd5476f6bb57b3368aa690c8d44
Ismael Mendoza <imendoza@umich.edu>
2022-05-11 14:55:49 -0400

    add slen to blend sim dataset

--- bliss/datasets/galsim_galaxies.py
+++ bliss/datasets/galsim_galaxies.py
@@ -401,6 +401,7 @@ class GalsimBlends(SingleGalsimGalaxies):
             "blendedness": blendedness,
             "n_sources": torch.tensor([n_sources]),
             "plocs": plocs,
+            "slen": slen,
         }
 
     def _sample_distance_from_center(self) -> Tensor:
